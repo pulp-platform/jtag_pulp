9d735762be712860f8215be9f9417c4835361f5f
Florent Rotenberg <florent.rotenberg@greenwaves-technologies.com>
2018-09-19 10:08:49 +0200

    Clean sv includes

--- src/jtag_axi_wrap.sv
+++ src/jtag_axi_wrap.sv
@@ -8,7 +8,6 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-`include "pulp_soc_defines.sv"
 
 module jtag_axi_wrap
   (
